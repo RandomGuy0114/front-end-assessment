ee1edd88d1885c9f395beba814bcbd71538f1162
RandomGuy0114 <clarenceperez686@gmail.com>
2023-04-19 20:51:23 +0800

    contacts

---
+++ src/components/contacts.vue
@@ -0,0 +1,18 @@
+<template>
+    <div class="contacts__container">
+        <h1 class="contacts__h1">
+        Want to see what Educat can do for you?
+    </h1>
+    <p  class="contacts__p">
+        Learn more by booking a demo today and discover why Educat is the only platform you'll ever need for your learning and training needs.
+    </p>
+    <button  class="contacts__btn">
+        LET’S TALK! 
+    </button>
+
+    <img class="contacts__cat" src="/src/assets/cat.svg" alt="">
+    <img class="contacts__paws" src="/src/assets/paws.svg" alt="">
+
+    </div>
+   
+</template>
