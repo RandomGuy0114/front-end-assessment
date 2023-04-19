556ab1dda05a321adc9bf32bae5486c141e45749
RandomGuy0114 <clarenceperez686@gmail.com>
2023-04-19 14:36:22 +0800

    importing the files

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,8 @@
 import { createApp } from 'vue'
-import './style.css'
+import './styles/style.css'
+import './styles/components/header.css'
+import './styles/components/landing-page.css'
+import './styles/components/products.css'
 import App from './App.vue'
 
 createApp(App).mount('#app')
