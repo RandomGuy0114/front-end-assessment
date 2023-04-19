f7c0bbd943100515bf53c52704e4990c2ea6c031
RandomGuy0114 <clarenceperez686@gmail.com>
2023-04-19 14:35:48 +0800

    home page

---
+++ src/components/home.vue
@@ -0,0 +1,34 @@
+<template>
+  <main class="landing__container">
+    <div class="landing__col1">
+      <h1 class="landing__h1">The LMS that's built for your needs</h1>
+      <p class="landing__p">
+        Experience seamless communication, easy data migration, and secured user
+        management in one awesome platform.
+      </p>
+      <button class="landing__btn1">go to lMS portal</button>
+    </div>
+    <div>
+      <div class="landing__img-animation">
+        <img class="landing__img" src="/src/assets/hero-banner.png" alt="" />
+      </div>
+    </div>
+  </main>
+  <div>
+    <button class="landing__btn2">
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        fill="none"
+        viewBox="0 0 24 24"
+        stroke-width="1.5"
+        stroke="currentColor"
+      >
+        <path
+          stroke-linecap="round"
+          stroke-linejoin="round"
+          d="M12 4.5v15m0 0l6.75-6.75M12 19.5l-6.75-6.75"
+        />
+      </svg>
+    </button>
+  </div>
+</template>
