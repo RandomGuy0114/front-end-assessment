4cef982a755514554f7d160246ff88d80f5b9399
RandomGuy0114 <clarenceperez686@gmail.com>
2023-04-19 14:36:05 +0800

    products page

---
+++ src/components/products.vue
@@ -0,0 +1,37 @@
+<template>
+  <section class="products__container">
+    <div class="products__row1">
+      <p class="produtcs__row1-p">OUR PRODUCTS</p>
+    </div>
+    <div class="products__row2">
+      <h1 class="produtcs__row2-h1">Learn without Limits</h1>
+    </div>
+    <div class="products__row3">
+      <div class="products__row3-rows">
+        <img src="/src/assets/product1.svg" alt="" />
+        <h4>Educat LMS</h4>
+        <p>
+          A Learning Management System and content library that offers a variety
+          of ways on how to learn through blended learning.
+        </p>
+      </div>
+      <div class="products__row3-rows">
+        <img src="/src/assets/product2.svg" alt="" />
+        <h4>Educat Library</h4>
+        <p>
+          An online library that contains Digital Modules, Worksheets and
+          Assessments aligned with DepEd’s K to 12 Curriculum
+        </p>
+      </div>
+      <div class="products__row3-rows">
+        <img src="/src/assets/product3.svg" alt="" />
+
+        <h4>Educat Rover</h4>
+        <p>
+          An offline solution run by mini-computers that are equipped with
+          wireless local area network for contactless file transfers.
+        </p>
+      </div>
+    </div>
+  </section>
+</template>
